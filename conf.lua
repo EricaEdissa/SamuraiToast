1996fbb25f815f2698d0b47006d9480b96db2fa6
Erica Edissa <edissa_tavares@hotmail.com>
2016-06-07 02:19:17 -0300

    configurate resource path by conf lua file

---
+++ conf.lua
@@ -0,0 +1,50 @@
+imagens = "recursos/"
+sons = "recursos/"
+texto = "recursos/"
+fontes = "recursos/"
+modulos = "modulos/"
+
+function adicionaCaminhoAlternativo(nomeDoArquivo, caminhoAlternativo)
+end
+
+function aplicaCaminhoAlternativo(nomeDoArquivo, caminhoAlternativo)
+	if not love.filesystem.exists(nomeDoArquivo) then -- se o arquivo nao foi encontrado
+		-- chame a funcao mudando para o caminho alternativo
+		nomeDoArquivo = caminhoAlternativo .. nomeDoArquivo
+	end
+	return nomeDoArquivo
+end
+
+require "love.graphics"
+require "love.audio"
+
+local oldNewImage = love.graphics.newImage
+function love.graphics.newImage(nomeDoArquivo)
+	return oldNewImage(aplicaCaminhoAlternativo(nomeDoArquivo, imagens))
+end
+
+local oldNewFont = love.graphics.newFont
+function love.graphics.newFont(nomeDoArquivo)
+	return oldNewFont(aplicaCaminhoAlternativo(nomeDoArquivo, fontes))
+end
+
+local oldNewSource = love.audio.newSource
+function love.audio.newSource(nomeDoArquivo, mode)
+	return oldNewSource(aplicaCaminhoAlternativo(nomeDoArquivo, sons), mode)
+end
+
+
+local tempFun = io.open
+function io.open(nomeDoArquivo)
+	return tempFun(aplicaCaminhoAlternativo(nomeDoArquivo, texto))
+end
+
+--adiciona a pasta de modulos ao pacote
+-- pega todos os modulos na pasta de modulos
+local pastasDosModulos = love.filesystem.getDirectoryItems(modulos)
+for _,pastaDoModulo in ipairs(pastasDosModulos) do
+	-- adiciona caminho nos pacotes do projeto
+	package.path = package.path .. ";" .. modulos .. pastaDoModulo
+end
+
+print "configuracao!"
