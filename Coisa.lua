bda649c93f05701f3a3fc1703dac8548366b103f
Erica Edissa <edissa_tavares@hotmail.com>
2016-06-26 03:48:06 -0300

    Desloca coisas

--- Coisa.lua
+++ Coisa.lua
@@ -1,32 +1,27 @@
-Coisa = {} -- tabela que representa qualquer coisa no jogo (funciona como classe)
+-- tabela que representa qualquer coisa no jogo (funciona como classe)
+-- valores padroes podem ser colocados aqui
+Coisa = {
+	posicaoX = 0, posicaoY = 0,
+	largura, altura,
+	imagem
+}
 
 -- funcao new guardada na tabela Coisa
 -- equivalente a Coisa.new = function () {...}
-function Coisa:new(posicaoX, posicaoY, largura, altura, imagem)    
+function Coisa:new(objeto)    
 	-- toda funcao com : ao inves de . tem um parametro self passado escondido
 	-- esse parametro self é o nome da tabela antes do : , neste caso
 	-- self é Coisa
-
-	-- cria tabela com todos os atributos
-	atributos = {
-		posicaoX, posicaoY,
-		largura, altura,
-		imagem
-	}
-
-	-- inicialização dos atributos
-	atributos.posicaoX = posicaoX
-	atributos.posicaoY = posicaoY
-	atributos.largura = largura
-	atributos.altura = altura
-	atributos.imagem = imagem
+	
+	-- se não for passado objeto então cria um vazio
+	objeto = objeto or {}
 
 	-- quando uma variável é chamada e ainda não foi declarada procura-se o valor em __index
-	-- agora self que ainda não foi declarado é Coisa
+	-- agora o quando a tablela não tiver __index o __index é Coisa, ou seja, o que não tiver procura em Coisa
 	self.__index = self
 
-	-- a tabela atributos tem os metodos de Coisa de agora em diante
-	return setmetatable(atributos, self)
+	-- a tabela instancia tem os metodos de Coisa de agora em diante
+	return setmetatable(objeto, self)
 end
 
 function Coisa:vaiParaPosicao(posicaoX, posicaoY)
@@ -34,27 +29,32 @@ function Coisa:vaiParaPosicao(posicaoX, posicaoY)
 	self.posicaoY = posicaoY
 end
 
+function Coisa:desloca(quantidadeX, quantidadeY)
+	self.posicaoX = self.posicaoX + quantidadeX
+	self.posicaoY = self.posicaoY + quantidadeY
+end
+
 function Coisa:moveParaCima(quantidade)
-	self.posicaoY = self.posicaoY - quantidade
+	self:desloca(0, -quantidade)
 end
 
 function Coisa:moveParaBaixo(quantidade)
-	self.posicaoY = self.posicaoY + quantidade
+	self:desloca(0, quantidade)
 end
 
 function Coisa:moveParaDireita(quantidade)
-	self.posicaoX = self.posicaoX + quantidade
+	self:desloca(quantidade, 0)
 end
 
 function Coisa:moveParaEsquerda(quantidade)
-	self.posicaoX = self.posicaoX - quantidade
+	self:desloca(-quantidade, 0)
 end
 
 function Coisa:desenha()
 	-- muda pincel para branco
 	love.graphics.setColor(255,255,255);
 	-- pinta imagem usando love2d
-	love.graphics.draw(self.image, self.posicaoX, self.posicaoY)
+	love.graphics.draw(self.imagem, self.posicaoX, self.posicaoY)
 end
 
 --[[
@@ -107,6 +107,3 @@ function Coisa:colidiu(outraCoisa)
 	-- se não está ao lado então colidiu
 	return true;
 end
-
--- retorna a Coisa para quem requisitar
-return Coisa
