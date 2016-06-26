46aebd0e5ebb343787852ce86fc275ef7b87bf27
Erica Edissa <edissa_tavares@hotmail.com>
2016-06-26 15:53:53 -0300

    Controle da dinamica, parar, pausar e despausar

--- Dinamica.lua
+++ Dinamica.lua
@@ -5,16 +5,32 @@ function Dinamica:new(duracao, atualizacao)
 	objeto.tempoEmExecucao = 0
 	objeto.duracao = duracao
 	objeto.atualiza = atualizacao or function () end
+	objeto.pausado = false
 	self.__index = self
 	return setmetatable(objeto, self)
 end
 
 function Dinamica:executa(dt)
-	self.tempoEmExecucao = self.tempoEmExecucao + dt
-	if self.duracao and self.tempoEmExecucao > self.duracao then return end
-	self.atualiza(dt) -- definido pelo usuario	
+	if not self.pausado then
+		if self.duracao and self.tempoEmExecucao > self.duracao then
+			self:pausa()
+			return
+		end
+		self.atualiza(dt) -- definido pelo usuario	
+		self.tempoEmExecucao = self.tempoEmExecucao + dt
+	end
 end
 
-function Dinamica:voltaDoInicio()
+function Dinamica:pausa()
+	self.pausado = true
+end
+
+function Dinamica:para()
+	self:pausa()
 	self.tempoEmExecucao = 0
 end
+
+function Dinamica:despausa()
+	self.pausado = false
+end
+
